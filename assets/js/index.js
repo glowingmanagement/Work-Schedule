7c5c68bc95c3cc35a95a26c4f2556591fc1b9919
Josh Holmes <josh@glowingmanagement.com>
2022-05-02 10:30:25 +0100

    timeblocks created

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,17 +11,28 @@ const weekDay = [
 
 // get current date and time
 const getCurrentDate = () => {
+  // get current day
   const day = moment().day();
   const displayDay = weekDay[day];
+  // get current date
   const date = moment().toString().split(" ");
   const displayDate = date[2] + " " + date[1] + " " + date[3];
   const toDisplay = displayDay + ", " + displayDate;
   $("#current-day").append(toDisplay);
-  return toDisplay;
+  return date[4];
 };
 
 // create timeblocks
-const createTimeblocks = () => {};
+const createTimeblocks = (timeHour) => {
+  $(`
+    <div class="time-block">
+    <span class="hour">
+      ${timeHour}
+    </span>
+    <textarea class="todo">To Do Items</textarea>
+    <button type="button" class="btn save-btn"><i class="fa-solid fa-floppy-disk save-icon"></i></button>
+    </div>`).appendTo(".container");
+};
 
 // highlight blocks depending on time
 const highlightBlocks = () => {};
@@ -30,4 +41,8 @@ const highlightBlocks = () => {};
 
 const storeInput = () => {};
 
-getCurrentDate();
+const time = getCurrentDate();
+createTimeblocks("7.00");
+createTimeblocks("8.00");
+createTimeblocks("9.00");
+createTimeblocks("10.00");
